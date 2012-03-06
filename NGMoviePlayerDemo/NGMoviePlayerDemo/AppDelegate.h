214c5e2e8e2e541d8f4bbc4960ed279e3e858794
Matthias Tretter <matthias.tretter@gmail.com>
2012-03-06 14:00:00 +0100

    Added empty Demo Project

---
+++ NGMoviePlayerDemo/NGMoviePlayerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NGMoviePlayerDemo
+//
+//  Created by Tretter Matthias on 06.03.12.
+//  Copyright (c) 2012 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NGMoviePlayerDemo/NGMoviePlayerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NGMoviePlayerDemo
+//
+//  Created by Tretter Matthias on 06.03.12.
+//  Copyright (c) 2012 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
